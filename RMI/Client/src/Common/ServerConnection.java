8fa44f54643b0f79adacb98edb1da4d49062a6cf
RobbeRDG <robbedegroeve@gmail.com>
2020-11-07 18:01:12 +0100

    splitted the server an client implementation in two projects

---
+++ RMI/Client/src/Common/ServerConnection.java
@@ -0,0 +1,17 @@
+package Common;
+
+import Common.Objects.ChatMessage;
+
+import java.io.Serializable;
+import java.rmi.Remote;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public interface ServerConnection extends Remote, Serializable {
+
+    void addUser(String userName, ClientListener clientListener) throws Exception;
+    void removeUser(String userName) throws Exception;
+    void sendMessage(ChatMessage message, UUID chatID) throws Exception;
+    void createChat(String userName, String chatName, ArrayList<String> subscribers) throws Exception;
+    ArrayList<String> getOnlineUsers() throws Exception;
+}
---
+++ RMI/Server/src/ServerSide/ServerController/ServerController.java
@@ -0,0 +1,15 @@
+package ServerSide.ServerController;
+
+import Common.ClientListener;
+import Common.Objects.ChatMessage;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public interface ServerController {
+    void addUser(String userName, ClientListener clientListener) throws Exception;
+    void removeUser(String userName) throws Exception;
+    ArrayList<String> getOnlineUsers() throws Exception;
+    void sendMessage(ChatMessage message, UUID chatId) throws Exception;
+    void createChat(String userName, String chatName, ArrayList<String> subscribers) throws Exception;
+}
