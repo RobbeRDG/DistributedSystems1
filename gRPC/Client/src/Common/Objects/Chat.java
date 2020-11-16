9161c2f5d9c46bfc36e1aa9018fe3c9d6a2f4ec4
RobbeRDG <robbedegroeve@gmail.com>
2020-11-16 16:23:22 +0100

    Created template for gRPC

---
+++ gRPC/Client/src/Common/Objects/Chat.java
@@ -0,0 +1,50 @@
+package Common.Objects;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Chat implements Serializable {
+    private static final long serialVersionUID = 3L;
+
+    private String name;
+    private UUID id;
+    private ArrayList<ChatMessage> messages;
+    private ArrayList<String> subscribers;
+
+
+    public Chat(String name, UUID id) {
+        this.name = name;
+        this.id = id;
+        messages = new ArrayList<>();
+        subscribers = new ArrayList<>();
+    }
+
+    public ArrayList<ChatMessage> getMessages() {
+        return messages;
+    }
+
+    public void addMessage(ChatMessage message) {
+        messages.add(message);
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public ArrayList<String> getSubscribers() {
+        return subscribers;
+    }
+
+    public void addSubscriber(String userName) {
+        subscribers.add(userName);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addSubscribers(ArrayList<String> subscribers) {
+        this.subscribers.addAll(subscribers);
+    }
+}
---
+++ gRPC/Server/src/Common/Objects/ChatUser.java
@@ -0,0 +1,13 @@
+package Common.Objects;
+
+public class ChatUser {
+    private String name;
+
+    public ChatUser(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
